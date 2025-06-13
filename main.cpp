f8fa44ffd564793ebd82586fcc7f7072135d78ca
sexymanalive <lyvanna544@gmail.com>
2025-06-13 08:00:27 +0000

    another updates

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@ cout<<"Welcome to the main program ! "<<endl; ''
 int main(){
 
     system("clear"); 
-    string username="jamesbond"; 
-    string password="007password"; 
+    string username="jamesbond" 
+    string password="007password" 
     cout<<"This is the main class of our application.\n";
     cout<<"Thank you!"<<endl; 
     return 0 ; 
